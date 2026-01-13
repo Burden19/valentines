9a7c4434c3f87a721cdec25569b0673be7802602
Ahmed <ahmedmbarek61@gmail.com>
2026-01-13 21:58:30 +0100

    Fix all TypeScript and ESLint errors

--- lib/weather.ts
+++ lib/weather.ts
@@ -1,4 +1,4 @@
-export async function getWeather(lat, lon) {
+export async function getWeather(lat: number, lon: number) {
     const key = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
     const res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`
